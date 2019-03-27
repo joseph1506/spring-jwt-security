f2965919522f19a80f43d02d16c6b20426598b47
joseph1506 <josephin.angel@gmail.com>
2019-03-27 05:30:04 +0530

    Jwt Config and Providers- Partial commit

--- src/main/java/com/joe/security/springjwtsecurity/config/JwtSecurityConfig.java
+++ src/main/java/com/joe/security/springjwtsecurity/config/JwtSecurityConfig.java
@@ -2,6 +2,7 @@ package com.joe.security.springjwtsecurity.config;
 
 import com.joe.security.springjwtsecurity.security.JwtAuthenticationProvider;
 import com.joe.security.springjwtsecurity.security.JwtAuthenticationTokenFilter;
+import com.joe.security.springjwtsecurity.security.JwtSuccessHandler;
 import io.jsonwebtoken.Jwt;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +10,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -30,7 +32,14 @@ public class JwtSecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public JwtAuthenticationTokenFilter authenticationTokenFilter(){
         JwtAuthenticationTokenFilter filter = new JwtAuthenticationTokenFilter();
+        filter.setAuthenticationManager(authenticationManager());
+        filter.setAuthenticationSuccessHandler(new JwtSuccessHandler());
+        return filter;
     }
 
-
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+    }
 }
--- src/main/java/com/joe/security/springjwtsecurity/security/JwtAuthenticationTokenFilter.java
+++ src/main/java/com/joe/security/springjwtsecurity/security/JwtAuthenticationTokenFilter.java
@@ -1,4 +1,13 @@
 package com.joe.security.springjwtsecurity.security;
 
+import org.springframework.security.authentication.AuthenticationManager;
+
 public class JwtAuthenticationTokenFilter {
+    public void setAuthenticationManager(AuthenticationManager authenticationManager) {
+
+    }
+
+    public void setAuthenticationSuccessHandler(JwtSuccessHandler jwtSuccessHandler) {
+
+    }
 }
